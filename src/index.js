ef4337dcd6bb3936290631d8112d9664cf3a89d1
mabay919 <54597981+mabay919@users.noreply.github.com>
2020-10-03 21:51:59 +0300

    feat: create the calculation of the max, min, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
+exports.min = function min(array) {
+    let res = 0;
+    if (array) {
+        if (array.length !== 0) {
+            res = array.sort((a, b) => b - a);
+            return res.pop();
+        }
+    }
+    return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let res;
+    if (array) {
+        if (array.length !== 0) {
+            res = array.sort((a, b) => a - b);
+            return res.pop();
+        }
+    }
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let res;
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    if (array) {
+        if (array.length !== 0) {
+            res = array.reduce((sum, item) => sum + item, 0);
+            let averageValue = res / array.length;
+            return averageValue;
+        }
+    }
+    return 0;
+};
